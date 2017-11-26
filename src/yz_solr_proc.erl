9be6067d5e4fd5a5d4d609eb900d2a462e44cf4d
Steven Joseph <steven@stevenjoseph.in>
2017-11-26 22:36:57 +1100

    iFix syntax error

--- src/yz_solr_proc.erl
+++ src/yz_solr_proc.erl
@@ -239,7 +239,7 @@ build_cmd(JavaPath, SolrPort, SolrJMXPort, Dir, TempDir) ->
     Port = "-Djetty.port=" ++ integer_to_list(SolrPort),
     CP = "-jar",
     CP2 = filename:join([YZPrivSolr, "start.jar"]),
-    Module = "--module=http"
+    Module = "--module=http",
     %% log4j.properties must be in the classpath unless a full URL
     %% (e.g. file://) is given for it, and we'd rather not put etc or
     %% data on the classpath, but we have to templatize the file to
